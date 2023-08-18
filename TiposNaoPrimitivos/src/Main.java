96bfc54cdeaf97ba1c697c8c41b618e04a75adf7
root <root@localhost.localdomain>
2023-08-18 13:54:09 -0300

    Tipos nao primitivos

---
+++ TiposNaoPrimitivos/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int num =2; //tipos primitivos são nativos da linguagem, por isso nao possuem metodos
+        Integer num2 = 4; //tipos não primitivos são chamadas de instancia que possuem metodos a ser modificados
+        // tipos primitivos são mais leves
+        // tipos não primitivos sao mais pesados
+
+        // o resultado é o mesmo
+        System.out.println(num);
+        System.out.println(num2);
+
+    }
+}
