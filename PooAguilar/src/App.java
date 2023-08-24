cd04322686182900607bf8fd9a45195b753e450f
root <root@localhost.localdomain>
2023-08-23 21:53:01 -0300

    poo aguilar

---
+++ PooAguilar/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Guitar fender = new Guitar();
+        fender.chords = 6;
+        fender.name = "Fender";
+    
+        Guitar ivanez = new Guitar();
+        ivanez.chords = 6;
+        ivanez.name = "Ivanez";
+
+        Users pedrin = new Users();
+
+    }
+
+}
---
+++ PooAguilar/src/Guitar.java
@@ -0,0 +1,12 @@
+//violao
+public class Guitar {
+    //propriedades
+    public String name; 
+    public int chords;
+
+    //comportamento
+    public void Play(){
+        System.out.println("Tocando guitarra !!");
+    }
+
+}
